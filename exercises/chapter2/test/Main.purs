78ce8b70787350fc1aac39a56c6d09633e7a5b65
Alessio Prestileo <alessioprestileo@gmail.com>
2021-01-21 10:21:26 +0100

    Implement leftoverCents

--- exercises/chapter2/test/Main.purs
+++ exercises/chapter2/test/Main.purs
@@ -1,9 +1,9 @@
 module Test.Main where
 
-import Prelude
-import Test.MySolutions
 import Effect (Effect)
 import Euler (answer)
+import Prelude (Unit, discard, negate)
+import Test.MySolutions (circleArea, diagonal, leftoverCents)
 import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Main (runTest)
@@ -26,7 +26,6 @@ main = do
         Assert.equal 3.141592653589793 (circleArea 1.0)
       test "radius 3" do
         Assert.equal 28.274333882308138 (circleArea 3.0)
-    {-  Move this block comment starting point to enable more tests
     suite "leftoverCents" do
       test "23" do
         Assert.equal 23 (leftoverCents 23)
@@ -34,5 +33,6 @@ main = do
         Assert.equal 56 (leftoverCents 456)
       test "-789" do
         Assert.equal (-89) (leftoverCents (-789))
+    {-  Move this block comment starting point to enable more tests
 
 -}
--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,6 +1,8 @@
 module Test.MySolutions where
 
 import Prelude
+
+import Data.Int (rem)
 import Math (sqrt, pi)
 
 diagonal :: Number -> Number -> Number
@@ -8,3 +10,6 @@ diagonal w h = sqrt (w * w + h * h)
 
 circleArea :: Number -> Number
 circleArea r = pi * r * r
+
+leftoverCents :: Int -> Int
+leftoverCents n = rem n 100
