4afbcba8a287b3f688c2917317b927d0a285e15c
Alessio Prestileo <alessioprestileo@gmail.com>
2021-01-27 11:17:36 +0100

    Implement isInBook

--- exercises/chapter3/test/Main.purs
+++ exercises/chapter3/test/Main.purs
@@ -1,10 +1,11 @@
 module Test.Main where
 
 import Prelude
-import Test.MySolutions (findEntryByStreet)
+
 import Data.AddressBook (AddressBook, Entry, emptyBook, findEntry, insertEntry)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import Test.MySolutions (findEntryByStreet, isInBook)
 import Test.Unit (TestSuite, suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Main (runTest)
@@ -66,7 +67,6 @@ main =
       test "Lookup missing" do
         Assert.equal Nothing
           $ findEntryByStreet "456 Nothing St." book
-    {-  Move this block comment starting point to enable more tests
     suite "Exercise - isInBook" do
       test "Check existing" do
         Assert.equal true
@@ -74,7 +74,8 @@ main =
       test "Check missing" do
         Assert.equal false
           $ isInBook "unknown" "person" book
-    test "Exercise - removeDuplicates" do
+    {-  Move this block comment starting point to enable more tests
+    suite "Exercise - removeDuplicates" do
       Assert.equal book
         $ removeDuplicates bookWithDuplicate
 
--- exercises/chapter3/test/MySolutions.purs
+++ exercises/chapter3/test/MySolutions.purs
@@ -1,12 +1,19 @@
 module Test.MySolutions where
 
 import Prelude
-import Data.AddressBook (Entry, AddressBook)
+
+import Data.AddressBook (AddressBook, Entry, findEntry)
 import Data.List (filter, head)
-import Data.Maybe (Maybe)
+import Data.Maybe (Maybe(..))
 
 genericCompare :: forall a b. Eq b => (a -> b) -> b -> (a -> Boolean) 
 genericCompare accessor target = (==) target <<< accessor
 
 findEntryByStreet :: String -> AddressBook -> Maybe Entry
 findEntryByStreet street = head <<< filter (genericCompare _.address.street street)
+
+isInBook :: String -> String -> AddressBook -> Boolean
+isInBook firstName lastName book = case found of
+  Just entry -> true
+  Nothing -> false
+  where found = findEntry firstName lastName book
