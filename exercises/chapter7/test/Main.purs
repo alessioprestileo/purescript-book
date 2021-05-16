81606912f274cb34036517744401fbe9898cae65
Alessio Prestileo <alessioprestileo@gmail.com>
2021-05-16 22:32:11 +0200

    Implement addMaybe, subMaybe, mulMaybe and divMaybe

--- exercises/chapter7/test/Main.purs
+++ exercises/chapter7/test/Main.purs
@@ -24,7 +24,6 @@ main :: Effect Unit
 main =
   runTest do
     runChapterExamples
-    {-  Move this block comment starting point to enable more tests
     suite "Exercise Group - Applicative and Effects" do
       suite "Exercise - Numeric operators that work with Maybe" do
         suite "addMaybe" do
@@ -46,6 +45,7 @@ main =
         test "divMaybe" do
           Assert.equal (Just 2)
             $ divMaybe (Just 5) (Just 2)
+    {-  Move this block comment starting point to enable more tests
       suite "Exercise - Numeric operators that work with Apply" do
         suite "addApply" do
           test "Maybe Just" do
--- exercises/chapter7/test/MySolutions.purs
+++ exercises/chapter7/test/MySolutions.purs
@@ -2,4 +2,17 @@ module Test.MySolutions where
 
 import Prelude
 
--- Note to reader: Add your solutions to this file
+import Control.Apply (lift2)
+import Data.Maybe (Maybe)
+
+addMaybe :: Maybe Int -> Maybe Int -> Maybe Int
+addMaybe n1 n2 = lift2 add n1 n2
+
+subMaybe :: Maybe Int -> Maybe Int -> Maybe Int
+subMaybe n1 n2 = lift2 sub n1 n2
+
+mulMaybe :: Maybe Int -> Maybe Int -> Maybe Int
+mulMaybe n1 n2 = lift2 mul n1 n2
+
+divMaybe :: Maybe Int -> Maybe Int -> Maybe Int
+divMaybe n1 n2 = lift2 div n1 n2
